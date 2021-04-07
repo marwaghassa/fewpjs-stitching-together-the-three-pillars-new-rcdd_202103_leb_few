e9bcf8ba2a028078ab958d649d83bac08fc12c04
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-07 10:55:50 +0000

    Automatically backed up by Learn

--- demo.js
+++ demo.js
@@ -14,7 +14,8 @@ let colorStates = {
 // Without JavaScript, clicking on these heart shapes does nothing. Uncomment
 // this code and refresh the demo page.
 
-// let articleHearts = document.querySelectorAll(".like-glyph");
+learn
+let articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
   let heart = e.target;
